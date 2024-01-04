e0da4f68d89f1ed9b927f3cf46501d531ee7f785
SikanderSD <78195281+Sikander-SD@users.noreply.github.com>
2024-01-04 21:45:42 +0530

    Update home.js

--- mobile/home/home.js
+++ mobile/home/home.js
@@ -108,8 +108,8 @@ window.addEventListener('scroll', e=> {
 class LCS{
   constructor(){
     this.prev = null;
-		if (localStorage.lcs){ const storage_lcs = JSON.parse(localStorage.lcs)}
-		else{ const storage_lcs = {like:[],comment:[],share:[]}
+		if (localStorage.lcs){ const storage_lcs = JSON.parse(localStorage.lcs) }
+		else{ const storage_lcs = {like:[],comment:[],share:[]} }
     LCS.update(storage_lcs);
   }
   

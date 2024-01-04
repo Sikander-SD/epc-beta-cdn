873aca761a66aa380ae79ef4852285cc17188a03
SikanderSD <78195281+Sikander-SD@users.noreply.github.com>
2024-01-04 18:11:30 +0530

    Update login.js

--- mobile/login/login.js
+++ mobile/login/login.js
@@ -175,7 +175,7 @@ function gmailAuth() {
       },
       body: JSON.stringify(data)
     })
-    .then(response => {//<- {userProfileData:{lcs,profile},userSettings,...} from server
+    .then(response => {//<- {userProfileData:{profile},userSettings,...} from server
       // redirect to authentication consent
       if (response.status == 302) response.text().then(url=>window.open(url,"_self"))
       
@@ -229,7 +229,7 @@ function OTPAuth() {
     },
     body: JSON.stringify(data)
   })
-  .then(response => {//<- {userProfileData:{lcs,profile},userSettings,...} from server
+  .then(response => {//<- {userProfileData:{profile},userSettings,...} from server
     if(!response.ok) throw Error(response.status+" "+response.statusText);
     response.json().then(d=>{
       // proceed if server sent the user's data

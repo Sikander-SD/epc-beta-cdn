144fc301a8a8ea1f6c47605bec6d2434f312be9c
SikanderSD <78195281+Sikander-SD@users.noreply.github.com>
2024-01-15 15:58:32 +0530

    Update searchTab.js

--- mobile/component/searchTab.js
+++ mobile/component/searchTab.js
@@ -1,7 +1,7 @@
 function searchTabHTML() {
     const cmt = document.createComment("   search device -Modal   ");
     const modal = document.createElement("div");
-    const attrs = {class:"modal fade",id:"search-deviceModal","back-button-key":"",tabindex:"-1","aria-hidden":"true"}
+    const attrs = {class:"modal fade",id:"search-deviceModal",tabindex:"-1","aria-hidden":"true"}
     Object.keys(attrs).forEach(k=>modal.setAttribute(k,attrs[k]));
 
     modal.innerHTML = `
@@ -9,9 +9,10 @@ function searchTabHTML() {
     <div class="modal-content">
       <button hidden type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       <div class="modal-body">
-            <form onsubmit="return false;"> <button type="submit" hidden></button>
+            <form onsubmit="return false;">
                   <input id="search-device" list="names" type="search" name="query" placeholder="Latest iPhone" minlength="3" maxlength="50" autocomplete="off" required>
                   <datalist id="names"></datalist>
+                  <button type="submit" style="  margin: 5% auto;  display: flex;">Go</button>
             </form>
             <p hidden>Network Error!</p>
       </div>

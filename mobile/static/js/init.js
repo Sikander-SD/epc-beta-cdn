90832c5428467452bc3f9b29cf2c879a935c206b
Sikander-SD <78195281+Sikander-SD@users.noreply.github.com>
2024-05-04 12:12:16 +0000

    updated

--- mobile/static/js/init.js
+++ mobile/static/js/init.js
@@ -132,16 +132,16 @@ window.addEventListener("DOMContentLoaded", () => {
 	if ("intro login".includes(THIS_PAGE)) return;
 	cookieStore.get("ws_token").then(e=>wsConnect(e.value));// WebSocket
 	// cookieStore.get("sse_token").then(e=>sseConnect(e.value));// SSE Event
-	setInterval(()=>{  WS_SSE.forEach(func => func(
-{data: JSON.stringify({
-        noti: [{
-          title: "Testing",
-          body: "Test good",
-          id: new Date().getTime()
-        }]
-        })}    
-  ))
-                },10000)
+// 	setInterval(()=>{  WS_SSE.forEach(func => func(
+// {data: JSON.stringify({
+//         noti: [{
+//           title: "Testing",
+//           body: "Test good",
+//           id: new Date().getTime()
+//         }]
+//         })}    
+//   ))
+//                 },10000)
 })
 
 // handle data recieved from server
